37e813c9dc40b9e425f49e95d5d38fc1f0c58a02
samumar43 <samumar43@gmail.com>
2020-06-29 03:55:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -32,6 +32,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array;
 }
 function removeElementFromEndOfArray(array) {
-  var newArray = array.slice(3);
+  var newArray = array.slice(0, 1);
   return newArray;
 }
